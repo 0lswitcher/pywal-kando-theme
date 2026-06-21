59d41d5a45e793269e4e9f14395f9fd1f98c884b
0lswitcher <manjarodistro@gmail.com>
2026-06-20 23:41:17 -0500

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+# install.sh - installs pywal-to-kando.sh from bash-scripts repo
+
+CONFIG_DIR=""
+
+# func to check if using flatpak install
+check_for_flatpak() {
+  FLATPAK_APPID="menu.kando.Kando"
+  flatpak info "$FLATPAK_APPID" >/dev/null 2>&1
+}
+
+# check install method
+echo "Checking Kando configuration directory..."
+if check_for_flatpak; then
+  CONFIG_DIR="$HOME/.var/app/menu.kando.Kando/config/kando"
+else
+  CONFIG_DIR="$HOME/.config/kando"
+fi
+echo "Kando's config directory determined as $CONFIG_DIR"
+
+echo "Cloning repo for nether-labels theme..."
+git clone https://github.com/0lswitcher/pywal-kando-theme.git
+
+echo "Moving nether-labels/ directory to $CONFIG_DIR/nether-labels"
+mv ./nether-labels "$CONFIG_DIR/menu-themes/"
+
+echo "Deleting leftover repository files/directory"
+rm -R ./pywal-kando-theme
+
+echo "Installing pywal-to-glance.sh..."
+curl -sLO https://raw.githubusercontent.com/0lswitcher/bash-scripts/refs/heads/main/scripts/pywal-to-kando.sh
+
+echo "Updating execution permissions..."
+chmod +x ./pywal-to-kando.sh
+
+echo "Moving script to ~/.local/bin..."
+echo "Feel free to relocate!"
+mkdir -p ~/.local/bin && mv ./pywal-to-kando.sh ~/.local/bin/
+
+echo "Installation complete!"
